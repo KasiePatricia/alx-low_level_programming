511d61bbaf7affb2f30bf20bbe92601caee6af5a
KasiePatricia <ugwukasiep@gmail.com>
2022-09-09 07:37:28 -0700

    Added 9-print_comb.c file

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <studio.h>
+/**
+ * Description: main - prints all possible combinations of single-digit numbers
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+		for (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
