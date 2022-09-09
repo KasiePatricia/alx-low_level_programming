2d9d2c3244c741fe7148b6fb0cbd611e9166f823
KasiePatricia <ugwukasiep@gmail.com>
2022-09-09 06:34:01 -0700

    Added 5-print_numbers.c file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * Description: main - print all digit number of base 10
+ * starting from zero, followed by a new line.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		printf("%d\n", n);
+	return (0);
+}
