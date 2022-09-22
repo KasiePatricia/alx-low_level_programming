5d8028e5440b9f4645e6406dd317c08083c1d877
KasiePatricia <ugwukasiep@gmail.com>
2022-09-22 15:33:53 -0700

    Correction on task 12

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -5,24 +5,22 @@
  * @b: buffer.
  * @size: size of buffer.
  */
+
 void print_buffer(char *b, int size)
 {
 	int byte, index;
-
 	for (byte = 0; byte < size; byte += 10)
 	{
 		printf("%08x: ", byte);
-
 		for (index = 0; index < 10; index++)
 		{
 			if ((index + byte) >= size)
-				printf(" ");
+				printf("  ");
 			else
 				printf("%02x", *(b + index + byte));
 			if ((index % 2) != 0 && index != 0)
-				printf("  ");
+				printf(" ");
 		}
-
 		for (index = 0; index < 10; index++)
 		{
 			if ((index + byte) >= size)
@@ -34,7 +32,6 @@ void print_buffer(char *b, int size)
 		}
 		if (byte >= size)
 			continue;
-
 		printf("\n");
 	}
 	if (size <= 0)
